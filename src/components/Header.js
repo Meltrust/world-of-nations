864971e774cf3e0fe7a32af4dffbfbddec6e55a4
Meltrust <original.mtapia@outlook.com>
2021-11-28 00:58:02 -0600

    Remove unused icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { FaAngleLeft, FaMicrophone, FaCog } from 'react-icons/fa';
+import { FaAngleLeft } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
@@ -9,8 +9,7 @@ const Header = () => (
       <FaAngleLeft className="ms-2 me-2 home-arrow-button" />
     </Link>
     <h1 className="col-9 fs-4 text-white">World of Nations</h1>
-    <FaMicrophone className="mx-0" />
-    <FaCog className="ms-2 me-3 text-white" />
+
   </nav>
 );
 
