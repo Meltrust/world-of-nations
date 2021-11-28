b4e937ba0a935219050c70c86b890bff99460bfd
Meltrust <original.mtapia@outlook.com>
2021-11-28 02:40:38 -0600

    Improve header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom';
 const Header = () => (
   <nav className="navbar fixed-top strong-pink ">
     <Link to="/" className="text-white">
-      <FaAngleLeft className="ms-2 me-2 home-arrow-button" />
+      <FaAngleLeft className="ms-2 ms-md-3 ms-lg-5 me-2 home-arrow-button" />
     </Link>
-    <h1 className="col-9 fs-4 text-white">World of Nations</h1>
+    <h1 className="col-10 me-4 me-md-5 me-lg-0 col-sm-10 col-lg-10 fs-4 text-white">World of Nations</h1>
 
   </nav>
 );
